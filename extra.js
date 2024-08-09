8874affc6558dec02aee92dc3b3d8ec5636d5677
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-08-09 19:52:10 +0530

    Create extra.js

---
+++ extra.js
@@ -0,0 +1,23 @@
+async function kushan() = {
+  return await conn.sendMessage(from, { text: teks ,
+contextInfo: {
+    mentionedJid: [ '' ],
+    groupMentions: [],
+    forwardingScore: 1,
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: '120363291446278735@newsletter',
+      serverMessageId: 127
+    },
+externalAdReply: { 
+title: '🌟 DARK SHAN MD 🌟',
+body: 'simple use',
+mediaType: 1,
+sourceUrl: "https://wa.me/94766943622" ,
+thumbnailUrl: 'https://telegra.ph/file/98b9cdf149a52bef8f4d5.jpg' ,
+renderLargerThumbnail: false,
+showAdAttribution: true
+}
+}}, { quoted: mek })
+}
+module.exports = { 
