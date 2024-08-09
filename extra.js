f48e840fcc498556782c06154ccebe9c84464220
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-08-09 20:35:01 +0530

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,22 +1,24 @@
-const kushan = {
+const kushan = async (teks) => {
+  return await conn.sendMessage(from, { text: teks ,
 contextInfo: {
     mentionedJid: [ '' ],
     groupMentions: [],
     forwardingScore: 1,
     isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: '120363291446278735@newsletter',
+      newsletterJid: '120363182681793169@newsletter',
       serverMessageId: 127
     },
 externalAdReply: { 
-title: '🌟 DARK SHAN MD 🌟',
-body: 'simple use',
+title: '𝗭𝗲𝗿𝗼-𝗧𝗪𝗢 𝗠𝗗 🍭',
+body: 'ᴀ ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ',
 mediaType: 1,
-sourceUrl: "https://wa.me/94766943622" ,
-thumbnailUrl: 'https://telegra.ph/file/98b9cdf149a52bef8f4d5.jpg' ,
+sourceUrl: "https://zerotwomd.me/" ,
+thumbnailUrl: 'https://raw.githubusercontent.com/vihangayt0/ZeroTwo-Uploads/main/bbb61bc283cc1891a9a3c.jpg' ,
 renderLargerThumbnail: false,
 showAdAttribution: true
 }
-}} 
+}}, { quoted: mek })
+}
 
 module.exports = { kushan }
